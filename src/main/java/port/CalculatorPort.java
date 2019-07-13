96fe00ea4deadb2e56c682019f05a0f155984d4f
Beata <beatainez.k@gmail.com>
2019-07-13 11:47:25 +0200

    added calculator port

--- src/main/java/port/CalculatorPort.java
+++ src/main/java/port/CalculatorPort.java
@@ -1,4 +1,8 @@
 package port;
 
 public interface CalculatorPort {
+     Double add(Double a, Double b);
+     Double subtract(Double a, Double b);
+     Double multiply(Double a, Double b);
+     Double divide(Double a, Double b);
 }
